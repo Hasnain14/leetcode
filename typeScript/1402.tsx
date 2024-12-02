4fe2138c2bd1a1a49facccf022c7e5b36c1f26c5
Hasnain14 <hasnainmahmud14@gmail.com>
2024-12-02 14:04:21 +0600

    02/12/24

--- typeScript/1402.tsx
+++ typeScript/1402.tsx
@@ -1,8 +1,17 @@
 function maxSatisfaction(satisfaction: number[]): number {
-    let maxSum = 0;
-    console.log(satisfaction.length);
+    let maxSum = 0, sum = 0;
+    let sorted = satisfaction.sort((a, b) => b - a);
+    for (let i = 0; i < sorted.length; i++) {
+        sum += sorted[i];
+        if (sum < 0) {
+            break;
+        } else {
+            maxSum += sum
+        }
+    }
+    // console.log(sorted);
     return maxSum;
 };
 
-let satisfaction = [4, 3, 2];
+let satisfaction = [-1, -8, 0, 5, -9];
 console.log(maxSatisfaction(satisfaction))
